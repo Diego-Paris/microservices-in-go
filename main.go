6b16c5ce731f48d6ece454221ffc01280f5ce13e
Diego-Paris <diego.paris@upr.edu>
2020-11-14 16:49:45 -0600

    Added /goodbye route and error checking

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,6 +11,21 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("hello world!")
+
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err == nil {
+			http.Error(w, "Oops, something went wrong", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+
+		fmt.Fprintf(w, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("goodbye world!")
 	})
 
 	http.ListenAndServe(":8080", nil)
